3b8d9492bd29871cc7cf7467ef38b28ea397ba01
Sean Chittenden <sean@chittenden.org>
2012-11-10 12:38:31 -0800

    Update main() to include additional examples and ways of stressing Example's Perfect Forwarding.
    Signed-off-by: Sean Chittenden <sean@chittenden.org>

--- reentrant-api/example_main.cpp
+++ reentrant-api/example_main.cpp
@@ -1,5 +1,6 @@
 #include <sysexits.h>
 
+#include <cassert>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
@@ -24,28 +25,60 @@ main(const int /*argc*/, const char** /*argv*/) {
 
   try {
     Example e;
-    const std::string a_const_str("bbbbbbb");
-    e.foo_set(a_const_str);
-    std::string a_str("cccccccc");
-    e.foo_set(a_str);
-    e.foo_set(std::string("ddddd"));
+    { // Test assignment from std::string
+      std::string str("cccccccc");
+      e.foo_set(str);
+      assert(e.foo() == "cccccccc");  // Value is the same
+      assert(str.empty());            // Stole the contents of a_str
+    }
+    { // Test assignment from a const std::string
+      const std::string const_str("bbbbbbb");
+      e.foo_set(const_str);
+      assert(const_str == "bbbbbbb");               // Value is the same
+      assert(const_str.c_str() != e.foo().c_str()); // Made a copy
+    }
+    {
+      // Test a const char[7] and a temporary std::string
+      e.foo_set("foobar");
+      e.foo_set(std::string("ddddd"));
+    { // Test char[7]
+      char buf[7] = {"foobar"};
+      e.foo_set(buf);
+      assert(e.foo() == "foobar");
+    }
+    { //// And a *char[] & const *char[]
+      auto buf = new char[7];
+      std::memcpy(buf, "foobar", 6);
+      buf[6] = '\0';
+      e.foo_set(buf);
+      const char* const_ptr = buf;
+      e.foo_set(const_ptr);
+      delete[] buf;
+      assert(e.foo() == "foobar");
+    }
+
     cout << "Example's bar capacity: " << e.bar_capacity() << endl;
     const std::size_t len = e.bar_capacity();
 
-    // Use unique_ptr to free the memory
+    // Use unique_ptr to automatically free buf
     std::unique_ptr<char[]> buf(new char[len +1]);
+
+    // Copy bar in to buf
     if (!e.bar(len, buf.get()))
       throw std::runtime_error("Unable to get bar");
-    buf[len] = '\0';
-    cout << "Example's bar value: " << buf.get() << endl;
+    buf[len] = '\0'; // Null terminate the C string
+    cout << endl << "foo and bar (a.k.a.) have different values:" << endl;
     cout << "Example's foo value: " << e.foo() << endl;
+    cout << "Example's bar value: " << buf.get() << endl;
 
-    // Set bar, which has a side effect of calling foo_set
-    buf[0] = 'a';
-    if (!e.bar_set(len, buf.get()))
+    // Set bar, which has a side effect of calling foo_set()
+    buf[0] = 'c'; buf[1] = buf[2] = '+'; buf[3] = '\0';
+    if (!e.bar_set(sizeof("c++") - 1, buf.get()))
       throw std::runtime_error("Unable to set bar");
-    cout << "Example's bar value: " << buf.get() << endl;
+
+    cout << endl << "foo and bar now have identical values but only one lock was acquired when setting:" << endl;
     cout << "Example's foo value: " << e.foo() << endl;
+    cout << "Example's bar value: " << buf.get() << endl;
   } catch (...) {
     return EX_SOFTWARE;
   }
